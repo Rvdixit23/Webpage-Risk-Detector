4b89de84e6e0f0f5b5bd59e49b9f446283db9481
Varun K J <40955517+ShhhBaby@users.noreply.github.com>
2020-01-02 16:05:45 +0530

    Documentaion

--- contentStorage/popup.js
+++ contentStorage/popup.js
@@ -11,12 +11,13 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     chrome.tabs.executeScript(null, {
       file: "getPagesSource.js"
     }, function() {
-      // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+      // non http or ssh sites
       if (chrome.runtime.lastError) {
         message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
       }
     });
-    console.log(message.innerText);
+    console.log(message.innerText);//message contains page source as string
+    //loads into div#message
   
   }
   
