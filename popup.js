02b7a6b76cf85b1a04474a3a7c8e4293642fff5d
Varun K J <40955517+ShhhBaby@users.noreply.github.com>
2020-01-02 21:00:49 +0530

    DOM rendering in console

--- popup.js
+++ popup.js
@@ -1,24 +1,32 @@
 chrome.runtime.onMessage.addListener(function(request, sender) {
-    if (request.action == "getSource") {
-      message.innerText = request.source;
+  if (request.action == "getSource") {
+   src = request.source;
+   
+   var doc = new DOMParser().parseFromString(src, "text/html");
+   console.log(doc);
+   
+  }
+  
+});
+
+function onWindowLoad() {
+
+  //var message = document.querySelector('#message');
+  
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+   //non http or ssh pages
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
     }
   });
   
-  function onWindowLoad() {
-  
-    var message = document.querySelector('#message');
-    
-    chrome.tabs.executeScript(null, {
-      file: "getPagesSource.js"
-    }, function() {
-      // non http or ssh sites
-      if (chrome.runtime.lastError) {
-        message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-      }
-    });
-    console.log(message.innerText);//message contains page source as string
-    //loads into div#message
-  
-  }
-  
-  window.onload = onWindowLoad;
+
+}
+var src = "";
+window.onload = onWindowLoad;
+console.log(src);
+///var count = document.getElementsByTagName('a');
+// console.log(count.length);
+//file: "getPagesSource.js"
