397daed398554866b4102dc55c3fb16cb74d9220
Rvdixit23 <rvdixit23@gmail.com>
2020-01-03 17:34:29 +0530

    Popup modified to show image percentage using JS

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 // const fs = require('fs')
 var blackListData = ["google", "amazon"];
+var imgBlackList = ["goog"];
 
 chrome.runtime.onMessage.addListener(function(request, sender) {
 
@@ -15,19 +16,22 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 
         var doc = new DOMParser().parseFromString(src, "text/html");
         anchors = doc.getElementsByTagName("a");
+        images = doc.getElementsByTagName("img");
         message.innerText = anchors.length;
 
         //Finding page title
         //Creating h1 element with class "title" and text
         title = doc.title;
         heading = document.createElement("h1");
+        //CHANGE CSS Class HERE
         heading.class = "title";
         heading.innerText = "Source Webpage : " + title;
 
         //Percentage risk tag
-        riskLine = document.createElement("p");
+        riskLine = document.createElement("h2");
+        //CHANGE CSS Class HERE
         riskLine.class = "riskText";
-        riskLine.innerText = "Risk Percentage : ";
+        riskLine.innerText = "Percentage of redirects to Google owned pages: ";
 
 
 
@@ -36,8 +40,8 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
         document.body.insertBefore(riskLine, message);
 
 
-        console.log(anchors);
-
+        // console.log(anchors);
+        // console.log(images);
 
         // var fileData;
         // fs.readFile("contentStorage/blacklist.txt", "utf-8", (err, data) => {
@@ -66,6 +70,35 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
             }
         }
         message.innerText = ((adCount / anchors.length) * 100).toString() + "%";
+
+
+        //% of images from the blackList
+        let imgAdCount = 0;
+        imgStuff = [];
+        for (let index = 0; index < images.length; index++) {
+            for (let blindex = 0; blindex < imgBlackList.length; blindex++) {
+                if (images[index].src.indexOf(imgBlackList[blindex]) != -1) {
+                    imgAdCount = imgAdCount + 1;
+                    imgStuff.push(images[index]);
+                    // console.log(imgStuff)
+                }
+            }
+        }
+
+        console.log("Image Ad count ", imgAdCount);
+
+        imgText = document.createElement("h2");
+        //CHANGE CSS Class HERE
+        imgText.class = "riskText";
+        imgText.innerText = "Images from Google : ";
+        document.body.appendChild(imgText);
+
+        imgText = document.createElement("div");
+        imgText.id = "imgMessage";
+        imgText.innerHTML = ((imgAdCount / images.length) * 100).toString() + "%";
+        document.body.appendChild(imgText);
+
+
     }
 
 
